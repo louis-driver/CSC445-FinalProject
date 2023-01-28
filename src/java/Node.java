7aa29a7b536055c901144fb5cc696a4b6fe660ec
jblakeman03 <jblakeman@murraystate.edu>
2023-01-28 16:00:35 -0600

    Initial push of Node and Test Class
    Implements a node and a basic test class to work on methods.

---
+++ src/java/Node.java
@@ -0,0 +1,70 @@
+public class Node
+{
+
+    private int color;
+    private int positionID;
+    private Node sibling1;
+    private Node sibling3;
+    private Node sibling5;
+    private Node sibling7;
+    private Node sibling9;
+    private Node sibling11;
+    private boolean isEdge;
+
+    public Node(int color, int ID, boolean edge)
+    {
+        this.color = color;
+        positionID = ID;
+        isEdge = edge; 
+    }
+
+    public void setSibling(Node Node, int sibNum)
+    {
+        if(sibNum == 1)
+            sibling1 = Node;
+        else if(sibNum == 3)
+            sibling3 = Node;
+        else if(sibNum == 5)
+            sibling5 = Node;
+        else if(sibNum == 7)
+            sibling7 = Node;
+        else if(sibNum == 9)
+            sibling9 = Node;
+        else if(sibNum == 11)
+            sibling11 = Node;
+    }
+
+    public void setColor(int color)
+    {
+        this.color = color;
+    }
+
+    public Node getSibling(int sibNum)
+    {
+    if(sibNum == 1)
+        return sibling1;
+    else if(sibNum == 3)
+        return sibling3;
+    else if(sibNum == 5)
+        return sibling5;
+    else if(sibNum == 7)
+        return sibling7;
+    else if(sibNum == 9)
+        return sibling5;
+    else
+        return sibling11;
+    }
+
+    public int getColor()
+    {
+        return color;
+    }
+
+    public boolean isEdge()
+    {
+        return isEdge;
+    }
+
+}
+
+
---
+++ src/java/NodeTest.java
@@ -0,0 +1,18 @@
+
+public class NodeTest {
+    
+    public static void main(String[] args)
+    {
+        Node node1 = new Node(1, 1, false);
+        Node node2 = new Node(0, 2, false);
+
+        node1.setSibling(node2, 1);
+        node2.setSibling(node1, 7);
+
+        System.out.println(node1.getColor());
+        System.out.println(node1.getSibling(1).getColor());
+
+
+    }
+}
+
