f7337644048de7d617f6b0dd8abff117d769f2f7
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-02-17 16:32:11 -0600

    Created class to manage the board space

---
+++ AbaloneGraph.java
@@ -0,0 +1,8 @@
+public class AbaloneGraph
+{
+    //Create array or arraylist of 91 nodes for the entire board
+    // 61 of the nodes will be playable nodes, the other 30 are border nodes
+
+    //iterate through list of nodes, then assign the sibling nodes to each one
+    
+}
