25e5e73d8689b27a4706ce1774d19a5c800d4f40
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-02-21 13:58:07 -0600

    setSibling works for top half of graph

--- AbaloneGraph.java
+++ AbaloneGraph.java
@@ -9,12 +9,14 @@ public class AbaloneGraph
     public static void main(String[] args)
     {
         AbaloneGraph graph = new AbaloneGraph();
+        for (int i = 0; i < 30; ++i)
+            graph.printSiblings(i);
     }
 
     public AbaloneGraph()
     {
         createGraph();
-        //this.setSiblings();
+        this.setSiblings();
         this.printNodes();
     }
 
@@ -63,7 +65,8 @@ public class AbaloneGraph
                         graph[currPosition] = new Node(0, currPosition, false);
                 }
 
-                System.out.println(graph[currPosition]);
+                //Uncomment to view nodes being created
+                //System.out.println(graph[currPosition]);
                 ++currPosition;
             }
             if (incrementing)
@@ -80,7 +83,7 @@ public class AbaloneGraph
         int numRows = 11;
         int currPosition = 0;
 
-        //TODO test sibling nodes to see if they start null so I can only set those with actual siblings
+        //TODO null siblings need not be set
         for (int i = 0; i < numRows; ++i)
         {
             for (int j = 0; j < rowSize; ++j)
@@ -90,29 +93,31 @@ public class AbaloneGraph
 
                 if (incrementing)
                 {
+                    //Visually lower sibling nodes are always set when rowSize is increasing
                     graph[currPosition].setSibling(graph[currPosition+rowSize], 7);
                     graph[currPosition].setSibling(graph[currPosition+rowSize+1], 5);
 
-                    if (rowSize==6)
+                    //The left-most node of the starting row only has its sibling on the right
+                    // side in addition to sibling 7 and 5
+                    if (rowSize==6 && j==0)
                     {
-                        graph[currPosition].setSibling(null, 1);
-                        graph[currPosition].setSibling(null, 11);
+                        graph[currPosition].setSibling(graph[currPosition+1], 3);
                     }
+                    //The right-most node of the starting row only has its sibling on the left
+                    // side in addition to sibling 7 and 5
+                    else if (rowSize==6 && j==rowSize-1)
+                    {
+                        graph[currPosition].setSibling(graph[currPosition+1], 9);
+                    }
+                    //Siblings 9 and 11 are null as the node is on the left edge of the graph
                     else if (j==0)
                     {
                         graph[currPosition].setSibling(graph[currPosition-rowSize+1], 1);
                         graph[currPosition].setSibling(graph[currPosition+1], 3);
-                        graph[currPosition].setSibling(graph[currPosition+rowSize+1], 5);
-                        graph[currPosition].setSibling(graph[currPosition+rowSize], 7);
-                        graph[currPosition].setSibling(null, 9);
-                        graph[currPosition].setSibling(null, 11);
                     }
+                    //Siblings 1 and 3 are null as the node is on the right edge of the graph
                     else if (j==rowSize-1)
                     {
-                        graph[currPosition].setSibling(null, 1);
-                        graph[currPosition].setSibling(null, 3);
-                        graph[currPosition].setSibling(graph[currPosition+rowSize+1], 5);
-                        graph[currPosition].setSibling(graph[currPosition+rowSize], 7);
                         graph[currPosition].setSibling(graph[currPosition-1], 9);
                         graph[currPosition].setSibling(graph[currPosition+1], 11);
                     }
@@ -120,8 +125,6 @@ public class AbaloneGraph
                     {
                         graph[currPosition].setSibling(graph[currPosition-rowSize+1], 1);
                         graph[currPosition].setSibling(graph[currPosition+1], 3);
-                        graph[currPosition].setSibling(graph[currPosition+rowSize+1], 5);
-                        graph[currPosition].setSibling(graph[currPosition+rowSize], 7);
                         graph[currPosition].setSibling(graph[currPosition-1], 9);
                         graph[currPosition].setSibling(graph[currPosition+1], 11);
                     }
@@ -149,6 +152,16 @@ public class AbaloneGraph
         {
             System.out.println(n);
         }
+    }
+
+    public void printSiblings(int nodePosition)
+    {
+        System.out.println("Node: " + nodePosition);
+        for (int i = 1; i <= 11; i+=2)
+        {
+            System.out.println("Sibling" + i + " " + graph[nodePosition].getSibling(i));
+        }
+        System.out.println();
     }
 
 }
