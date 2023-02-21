ddc81e1197abd61d6ad086a4a82477db3bacc9b7
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-02-20 21:24:02 -0600

    Fixing bugs in createGraph()

--- AbaloneGraph.java
+++ AbaloneGraph.java
@@ -1,8 +1,90 @@
 public class AbaloneGraph
 {
+    private Node[] graph = new Node[91];
     //Create array or arraylist of 91 nodes for the entire board
     // 61 of the nodes will be playable nodes, the other 30 are border nodes
 
     //iterate through list of nodes, then assign the sibling nodes to each one
-    
+
+    public static void main(String[] args)
+    {
+        AbaloneGraph graph = new AbaloneGraph();
+    }
+
+    public AbaloneGraph()
+    {
+        createGraph();
+        //this.setSiblings();
+        this.printNodes();
+    }
+
+    private void createGraph()
+    {
+        boolean incrementing = true;
+        int rowSize = 6;
+        int numRows = 11;
+        int currPosition = 0;
+
+        for (int i = 0; i < numRows; ++i)
+        {
+            for (int j = 0; j < rowSize; ++j)
+            {
+                if (rowSize == 11)
+                    incrementing = false;
+
+                if (incrementing)
+                {
+                    //Starting edge of board or an edge along the side
+                    if (rowSize==6 || j==0 || j==rowSize-1)
+                        graph[currPosition] = new Node(0, currPosition, true);
+                    //Starting row for Player2's pieces
+                    else if (rowSize==7 || rowSize==8)
+                        graph[currPosition] = new Node(2, currPosition, false);
+                    //Extra three pieces in middle of the third row out
+                    else if (rowSize==9 && (j==3 || j==4 || j==5))
+                        graph[currPosition] = new Node(2, currPosition, false);
+                    //Any other empty board space that is not an edge
+                    else
+                        graph[currPosition] = new Node(0, currPosition, false);
+                }
+                else //I.e. Player1/Bottom side of the board
+                {
+                    //Starting edge of board or an edge along the side
+                    if (rowSize==6 || j==0 || j==rowSize-1)
+                        graph[currPosition] = new Node(0, currPosition, true);
+                    //Starting row for Player1's pieces
+                    else if (rowSize==7 || rowSize==8)
+                        graph[currPosition] = new Node(1, currPosition, false);
+                    //Extra three pieces in middle of the third row out
+                    else if (rowSize==9 && (j==3 || j==4 || j==5))
+                        graph[currPosition] = new Node(1, currPosition, false);
+                    //Any other empty board space that is not an edge
+                    else
+                        graph[currPosition] = new Node(0, currPosition, false);
+                }
+
+                System.out.println(graph[currPosition]);
+                if (incrementing)
+                    ++rowSize;
+                else
+                    --rowSize;
+
+                ++currPosition;
+            }
+        }
+    }
+
+    private void setSiblings()
+    {
+
+    }
+
+    public void printNodes()
+    {
+        for (Node n : graph)
+        {
+            System.out.println(n);
+        }
+    }
+
 }
