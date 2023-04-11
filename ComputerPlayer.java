4752e05778619664650890fa6474c3b4de0a4375
jblakeman03 <jblakeman@murraystate.edu>
2023-04-11 13:02:28 -0500

    create computer player

---
+++ ComputerPlayer.java
@@ -0,0 +1,17 @@
+//A class to play against a human 
+//It will probably play like the biggest idiot known to man kind, so be nice. 
+
+public class ComputerPlayer {
+    private Node[] playersNodes;
+
+    public ComputerPlayer(Node[] n)
+    {
+        playersNodes = n;
+    }
+
+    public int[] getMove()
+    {
+        r
+    }
+
+}
