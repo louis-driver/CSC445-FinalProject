dfcf26a9c392937f0345f4f95e5ef5c95719e986
bfloyd3 <125699485+bfloyd3@users.noreply.github.com>
2023-04-22 22:47:42 -0500

    Fixed RulesPanel, scrollpane (no private properties), added goodbye message
    -Fixed RulesPanel, now its own file and includes a scrollpane
    -Don't make anything in RulesPanel private, the ComponentListener is still in AbaloneMenu and AbaloneMenu also needs access to the scrollpane
    -Added goodbye message that shows when the user presses any of the quit program buttons

--- RulesPanel.java
+++ RulesPanel.java
@@ -1,131 +1,86 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.event.ComponentEvent;
-
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
-// A class to display the abalone rules in a JPanel
+//A class to display the abalone rules in a JPanel
 
 public class RulesPanel extends JPanel
 {
-    JScrollPane scrollPane;
-    JPanel rules = new JPanel();
-    JButton menuButton;
-    String[] ruleStrings;
-	private Font titleFont = new Font("Times New Roman", Font.ITALIC, 50);
-	private Font headerFont = new Font("Times New Roman", Font.BOLD, 30);
-	
-	private Color BoardColorLight = new Color(160, 130, 105);
-	private Color boardColorDark = new Color(75, 45, 30);
-    
-    //Test Main class
-    public static void main(String[] args)
-    {
-        
-        int frameWidth = 12000;
-        int frameHeight = 800;
-        JFrame frame = new JFrame();
-		JButton menuBtn = new JButton("Back to Menu");
-        RulesPanel panel = new RulesPanel(frame, menuBtn);
+    JScrollPane RulesScrollPane;
+    JLabel rulesTitle;
+    JLabel[] ruleLabels;
+    static Color BoardColorLight = new Color(160, 130, 105);
+	static Color BoardColorDark = new Color(75, 45, 30);
 
-        frame.setSize(frameWidth, frameHeight);
-        frame.setTitle("Rules");
-        //frame.add(panel);
-        
-		panel.setSize(frame.getSize());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(true);
-        frame.setVisible(true);
-        frame.revalidate();
-        frame.repaint();
-    }
-
-    public RulesPanel(JFrame frame, JButton toMenuBtn)
+    public RulesPanel()
     {
-        this.frame = frame;
-        menuButton = toMenuBtn;
-		rules.setLayout(new BoxLayout(rules, BoxLayout.Y_AXIS));
-		rules.setBackground(BoardColorLight);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBackground(BoardColorLight);
+		setMinimumSize(new Dimension(getWidth(), getHeight()+500));
 		
-		JLabel rulesTitle = new JLabel(" RULES OF ABALONE");
-		rulesTitle.setFont(titleFont);
+		rulesTitle = new JLabel(" RULES OF ABALONE");
+		rulesTitle.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 50));
 		rulesTitle.setForeground(Color.BLACK);
+		add(Box.createRigidArea(new Dimension(0,20)));
+		add(rulesTitle);
+		
+		ruleLabels = new JLabel[23];
+		ruleLabels[0] = new JLabel("   General ");
+		ruleLabels[1] = new JLabel("       - Your objective is to push 6 of your opponent's pieces off the board.");
+		ruleLabels[2] = new JLabel("       - You may move up to three adjacent pieces, positioned in a straight line, in one direction.");
+		ruleLabels[3] = new JLabel("       - This direction can be either 'in-line' (parallel to your line of selected pieces).");
+		ruleLabels[4] = new JLabel("          or 'broadside' (perpendicular to the line).");
+		ruleLabels[5] = new JLabel("   How to Move");
+		ruleLabels[6] = new JLabel("       - For an in-line move, left-click the piece at the beginning of the line you wish to move.");
+        ruleLabels[7] = new JLabel("./Images/SecondClickInline.png");
+		ruleLabels[8] = new JLabel("       - Then, right-click the board space that neighbors the selected piece in the direction you wish to move");
+        ruleLabels[9] = new JLabel("./Images/InlineMoveMadeSmall.png");
+        ruleLabels[10] = new JLabel("       - For a broadside move, select two or three pieces that are in a continuous line.");
+        ruleLabels[11] = new JLabel("./Images/SelectMultiple2Small.png");
+        ruleLabels[12] = new JLabel("       - Then, right-click on a space neighboring either end of the selected line of pieces.");
+        ruleLabels[13] = new JLabel("./Images/BroadsideRight2Small.png");
+        ruleLabels[14] = new JLabel("       - NOTE: You should not right-click spaces that border the middle piece in the selected line of pieces;");
+        ruleLabels[15] = new JLabel("           Because the middle spaces make which direction to move in ambiguous, you may not always move the way you intended.");
+        ruleLabels[16] = new JLabel("   Pushing Your Opponent");
+        ruleLabels[17] = new JLabel("       - To push an opponent's piece(s), simply make an in-line move in which your pieces outnumber your opponent's.");
+        ruleLabels[18] = new JLabel("./Images/BeforePushSmall.png");
+        ruleLabels[19] = new JLabel("./Images/AfterPushSmall.png");
+        ruleLabels[20] = new JLabel("       - Just remember: 3 pieces push 2, and 2 pieces push 1.");
+        ruleLabels[21] = new JLabel("       - 1 single piece can't push another (since 1 is not greater than 1), which means broadside moves can't push opponents.");
+        ruleLabels[22] = new JLabel("   Have fun!");
 
-		toMenuBtn.setForeground(Color.white);
-		toMenuBtn.setBackground(Color.RED);
-
-        rules.add(Box.createRigidArea(new Dimension(0,20)));
-		rules.add(rulesTitle);
-
-        setRuleStrings();
-        createLabels(ruleStrings);
-
-		rules.add(Box.createRigidArea(new Dimension(0,7)));
-		rules.add(toMenuBtn);
-        scrollPane = new JScrollPane(this);
-        scrollPane.setEnabled(true);
-        scrollPane.createHorizontalScrollBar();
-        this.add(rules);
-        //this.add(scrollPane);
-        scrollPane.setBackground(BoardColorLight);
-        frame.add(scrollPane);
-    }
-
-    private void createLabels(String[] strings)
-    {
-        ruleLabels = new JLabel[strings.length];
-        for (int i = 0; i < strings.length; ++i)
+		for (int i = 0; i < ruleLabels.length; ++i)
         {
             //Image Path found
-            if (strings[i].charAt(0) == '.')
+            if (ruleLabels[i].getText().charAt(0) == '.')
             {
-                ImageIcon rulesImage = new ImageIcon(strings[i]);
+                ImageIcon rulesImage = new ImageIcon(ruleLabels[i].getText());
                 ruleLabels[i] = new JLabel(rulesImage);
                 ruleLabels[i].setMaximumSize(new Dimension(350, 350));
-
             }
             //Text Labels
+            //Headers
+            else if (ruleLabels[i].getText().charAt(3) != ' ')
+            {
+                add(Box.createRigidArea(new Dimension(0,25)));
+                ruleLabels[i] = new JLabel(ruleLabels[i].getText());
+                ruleLabels[i].setForeground(Color.BLACK);
+				ruleLabels[i].setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 20));
+            }
+            //Normal text
             else
             {
-                rules.add(Box.createRigidArea(new Dimension(0,10)));
-                ruleLabels[i] = new JLabel(strings[i]);
+                add(Box.createRigidArea(new Dimension(0,10)));
+                ruleLabels[i] = new JLabel(ruleLabels[i].getText());
                 ruleLabels[i].setForeground(Color.BLACK);
-                if (strings[i].length() < 30)
-                    ruleLabels[i].setFont(headerFont);
-                else    
-                    ruleLabels[i].setFont(textFont);
+				ruleLabels[i].setFont(new Font("Times New Roman", Font.ITALIC, 30));
             }
-            rules.add(ruleLabels[i]);
-        }
-    }
-
-    private void setRuleStrings()
-    {
-        ruleStrings = new String[22];
-		ruleStrings[0] = "   General ";
-		ruleStrings[1] = "      - Your objective is to push 6 of your opponent's marbles off the board.";
-		ruleStrings[2] = "      - You may move up to three adjacent pieces, positioned in a straight line, in one direction.";
-		ruleStrings[3] = "      - This direction can be either 'broadside' (parallel to your line of selected pieces).";
-		ruleStrings[4] = "          or 'inline' (perpendicular to the line).";
-		ruleStrings[5] = "   How to Move";
-		ruleStrings[6] = "       - For an inline move, left-click the piece at the beginning of the line you wish to move.";
-        ruleStrings[7] = "./Images/SecondClickInline.png";
-		ruleStrings[8] = "       - Then right-click the board space that neighbors the selected piece in the direction you wish to move";
-        ruleStrings[9] = "./Images/InlineMoveMadeSmall.png";
-        ruleStrings[10] = "       - For a broadside move, select two or three pieces that are in a continuous line.";
-        ruleStrings[11] = "./Images/SelectMultiple2Small.png";
-        ruleStrings[12] = "       - Then right-click on a space neighboring either end of the selected line of pieces.";
-        ruleStrings[13] = "./Images/BroadsideRight2Small.png";
-        ruleStrings[14] = "       - NOTE: You should not right-click spaces that border the middle of the selected line of pieces.";
-        ruleStrings[15] = "           because the middle spaces provide an ambiguous direction to move in, will be choosen for you.";
-        ruleStrings[16] = "   Pushing Your Opponent";
-        ruleStrings[17] = "       - To push an opponent's piece(s) simply make an inline move in which your pieces outnumber your opponent's.";
-        ruleStrings[18] = "./Images/BeforePushSmall.png";
-        ruleStrings[19] = "./Images/AfterPushSmall.png";
-        ruleStrings[20] = "       - Simply remember: 3 pushes 2 & 2 pushes 1";
-        ruleStrings[21] = "       - Given that 1 single piece can't push another (as 1 is not greater than 1), broadside moves can't push opponents.  ";
+            add(ruleLabels[i]);
+		}
+        add(Box.createRigidArea(new Dimension(0,25)));
+		
+        RulesScrollPane = new JScrollPane(this);
+		RulesScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
     }
 }
