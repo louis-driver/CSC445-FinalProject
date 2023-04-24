dad2ce9d710e87bc1d21a5e77520556b12e5c70f
bfloyd3 <125699485+bfloyd3@users.noreply.github.com>
2023-04-24 18:22:06 -0500

    Comments, restructuring, new panel
    - Added more comments
    - Trimmed and restructured code
    - Quit buttons now display a dedicated panel instead of the main menu without buttons

--- src/AbaloneMenu.java
+++ src/AbaloneMenu.java
@@ -1,144 +1,137 @@
-package src;
 //Bryan Floyd
 //Abalone Menu
 
+package src;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class AbaloneMenu extends JFrame
 {
-	private static JPanel menuPanel;
-	private JPanel buttonSectionPanel;
-	private AbalonePanel displayPanel = new AbalonePanel(new AbaloneGraph(), false, true);
-<<<<<<< HEAD
-=======
-	//private AbalonePanel abalonePanelD = new AbalonePanel(AbaloneGraphD, true);
->>>>>>> main
-	private AbalonePanel abalonePanelS;
-	private AbalonePanel abalonePanelM;
-	private AbaloneGraph abaloneGraphS;
-	private AbaloneGraph abaloneGraphM;
-	private RulesPanel rulesPanel;
-	private JScrollPane rulesScrollPane;
-	private JLabel title;
-	private JLabel imageLabel;
-	private JButton spButton;
-	private JButton mpButton;
-	private JButton rulesButton;
-	private JButton quitButton;
-	private JButton backToMenuButton;
+	private JPanel menuPanel = new JPanel();
+
+	private JLabel titleLabel = new JLabel("ABALONE", SwingConstants.CENTER);
+	private JButton[] menuButtons = new JButton[4];
+	private JPanel[] buttonPanels = new JPanel[4];
+	private JPanel buttonSectionPanel = new JPanel();
+
+	private AbalonePanel abalonePanelS = new AbalonePanel(new AbaloneGraph(), true, false);
+	private AbalonePanel abalonePanelM = new AbalonePanel(new AbaloneGraph(), false, false);
 	private JMenuBar gameOptionsBar = new JMenuBar();
-	private JMenu gameOptionsMenu = new JMenu("Options");
-	private JMenu gameQuitMenu = new JMenu("Quit to...");
-	private JMenuItem showRulesItem = new JMenuItem("Rules");
-	private JMenuItem gameResetItem = new JMenuItem("Reset Game");
-	private JMenuItem quitGameItem = new JMenuItem("Menu");
-	private JMenuItem quitProgramItem = new JMenuItem("Desktop");
-	
-	
-	private static Color boardColorLight = new Color(160, 130, 105);
-	private static Color boardColorDark = new Color(75, 45, 30);
-	private static Dimension screenSize = new Dimension((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight()-40);
-	private static int screenWidth = (int) screenSize.getWidth();
-	private static int screenHeight = (int) screenSize.getHeight();
 	private boolean spGameInProgress = false;
 	private boolean mpGameInProgress = false;
-	private Sound sound = new Sound();
 
+	private RulesPanel rulesPanel = new RulesPanel();
+	private JScrollPane rulesScrollPane = new JScrollPane(rulesPanel);
+
+	private JPanel goodByePanel = new JPanel();
+	private JLabel goodByeLabel = new JLabel("Thanks for playing!", SwingConstants.CENTER);
+	
+	private Color boardColorLight = new Color(160, 130, 105);
+	private Color boardColorDark = new Color(75, 45, 30);
+
+	//Constructor
 	public AbaloneMenu()
 	{			
-		setPreferredSize(getSize());
-		setBackground(new Color(160, 130, 105));
-		
-		menuPanel = new JPanel();
+		//Setting up panels and in-game menu bar
+		menuPanelSetup();
+		rulesPanelSetup();
+		gameMenuSetup();
+		goodByePanelSetup();
+
+		//Setting up JFrame
+		setContentPane(menuPanel);
+		setTitle("Abalone");
+		setSize(new Dimension((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight()-30));
+		setMinimumSize(new Dimension((int)(Toolkit.getDefaultToolkit().getScreenSize().getWidth()/3), (int)(Toolkit.getDefaultToolkit().getScreenSize().getHeight()*0.85)));
+		setBackground(boardColorLight);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addComponentListener(componentListener);
+		setVisible(true);
+	}
+	//Main method
+	public static void main(String[] args)
+	{
+		AbaloneMenu AbaloneMenu = new AbaloneMenu();
+	}
+	
+	//Set up main menu
+	private void menuPanelSetup()
+	{
+		//Set menuPanel appearance
 		menuPanel.setLayout(new BorderLayout());
 		menuPanel.setBackground(boardColorLight);
 		
-		JPanel titlePanel = new JPanel();
-		titlePanel.setBackground(boardColorLight);
-		JPanel spBPanel = new JPanel();
-		spBPanel.setBackground(new Color(75, 45, 30));
-		spBPanel.setLayout(new BorderLayout());
-		JPanel mpBPanel = new JPanel();
-		mpBPanel.setBackground(new Color(75, 45, 30));
-		mpBPanel.setLayout(new BorderLayout());
-		JPanel rulesBPanel = new JPanel();
-		rulesBPanel.setBackground(new Color(75, 45, 30));
-		rulesBPanel.setLayout(new BorderLayout());
-		JPanel quitBPanel = new JPanel();
-		quitBPanel.setBackground(new Color(75, 45, 30));
-		quitBPanel.setLayout(new BorderLayout());
+		//Set titleLabel Appearance
+		titleLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, getHeight()/10));
+		titleLabel.setForeground(boardColorDark);
+
+		//Instance of the game panel for display on the main menu
+		AbalonePanel abalonePanelD = new AbalonePanel(new AbaloneGraph(), false, true);
+
+		//Initialize main menu buttons
+		menuButtons[0] = new JButton("Singleplayer");
+		menuButtons[1] = new JButton("Multiplayer");
+		menuButtons[2] = new JButton("Rules");
+		menuButtons[3] = new JButton("Quit");
+		for(int i=0; i<buttonPanels.length; i++)
+		{
+			menuButtons[i].addActionListener(actionListener);
+			menuButtons[i].setForeground(Color.WHITE);
+			menuButtons[i].setBackground(boardColorDark);
+		}
+		menuButtons[3].setForeground(Color.RED);
 		
-		ImageIcon boardImage = new ImageIcon("./Resources/Images/MainMenuImage.png");
-		imageLabel = new JLabel(boardImage);
-		
-		title = new JLabel("ABALONE", SwingConstants.CENTER);
-		title.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, getHeight()/10));
-		title.setAlignmentX(CENTER_ALIGNMENT);
-		
-		spButton = new JButton("Singleplayer");
-		mpButton = new JButton("Multiplayer");
-		rulesButton = new JButton("Rules");
-		quitButton = new JButton("Quit");
-		
-		title.setForeground(new Color(75, 45, 30));
-		spButton.setForeground(Color.WHITE);
-		mpButton.setForeground(Color.WHITE);
-		rulesButton.setForeground(Color.WHITE);
-		quitButton.setForeground(Color.RED);
-		
-		spButton.setBackground(new Color(75, 45, 30));
-		mpButton.setBackground(new Color(75, 45, 30));
-		rulesButton.setBackground(new Color(75, 45, 30));
-		quitButton.setBackground(new Color(75, 45, 30));
-		
-		//Add title
-		titlePanel.add(title);
-		menuPanel.add(titlePanel, BorderLayout.NORTH);
-		
-		//Add picture of board
-		menuPanel.add(displayPanel, BorderLayout.CENTER);
-		
-		//Add panels containing buttons, add spacers, add size bounds
-		Dimension buttDimension = new Dimension(250, 40);
-		
-		buttonSectionPanel = new JPanel();
+		//Initialize panel the main menu buttons will go on
 		buttonSectionPanel.setLayout(new BoxLayout(buttonSectionPanel, BoxLayout.PAGE_AXIS));
 		buttonSectionPanel.setOpaque(false);
 
-		spBPanel.add(spButton);
-		buttonSectionPanel.add(spBPanel);
-		spBPanel.setMinimumSize(buttDimension);
-		spBPanel.setMaximumSize(buttDimension);
-		buttonSectionPanel.add(Box.createRigidArea(new Dimension(0,10)));
-		mpBPanel.add(mpButton);
-		buttonSectionPanel.add(mpBPanel);
-		mpBPanel.setMinimumSize(buttDimension);
-		mpBPanel.setMaximumSize(buttDimension);
-		buttonSectionPanel.add(Box.createRigidArea(new Dimension(0,10)));
-		rulesBPanel.add(rulesButton);
-		buttonSectionPanel.add(rulesBPanel);
-		rulesBPanel.setMinimumSize(buttDimension);
-		rulesBPanel.setMaximumSize(buttDimension);
-		buttonSectionPanel.add(Box.createRigidArea(new Dimension(0,10)));
-		quitBPanel.add(quitButton);
-		buttonSectionPanel.add(quitBPanel);
-		quitBPanel.setMinimumSize(buttDimension);
-		quitBPanel.setMaximumSize(buttDimension);
-		buttonSectionPanel.add(Box.createRigidArea(new Dimension(0,7)));
-		buttonSectionPanel.add(Box.createVerticalGlue());
-		buttonSectionPanel.add(Box.createHorizontalGlue());
+		//Add each main menu button to its own panel to make it center properly in BoxLayout
+		//Set constant size for button panels
+		//Add button panels to a panel to be added to the menu
+		for(int i=0; i<buttonPanels.length; i++)
+		{
+			buttonPanels[i] = new JPanel();
+			buttonPanels[i].setLayout(new BorderLayout());
+			buttonPanels[i].setOpaque(false);
+			buttonPanels[i].add(menuButtons[i]);
+			buttonPanels[i].setMinimumSize(new Dimension(250, 40));
+			buttonPanels[i].setMaximumSize(new Dimension(250, 40));
 
+			buttonSectionPanel.add(buttonPanels[i]);
+			buttonSectionPanel.add(Box.createRigidArea(new Dimension(0,10)));
+		}
+
+		//Build main menu panel
+		menuPanel.add(titleLabel, BorderLayout.NORTH);
+		menuPanel.add(abalonePanelD, BorderLayout.CENTER);
 		menuPanel.add(buttonSectionPanel, BorderLayout.SOUTH);
-		
-		spButton.addActionListener(MainListener);
-		mpButton.addActionListener(MainListener);
-		rulesButton.addActionListener(MainListener);
-		quitButton.addActionListener(MainListener);
-		
-		add(menuPanel);
+	}
 
+	//Add additional button to return to previous panel
+	private void rulesPanelSetup()
+	{
+		//Back button takes player from RulesPanel to their previous panel
+		JButton rulesBackButton = new JButton("Back");
+		rulesBackButton.setForeground(Color.RED);
+		rulesBackButton.setBackground(boardColorDark);
+		rulesBackButton.addActionListener(actionListener);
+		rulesPanel.add(rulesBackButton);
+	}
+	
+	//Set up JMenuBar to be displayed during game
+	//ActionListener will add and remove JMenuBar as needed
+	private void gameMenuSetup()
+	{
+		//JMenuBar components
+		JMenu gameOptionsMenu = new JMenu("Options");
+		JMenu gameQuitMenu = new JMenu("Quit to...");
+		JMenuItem showRulesItem = new JMenuItem("Rules");
+		JMenuItem gameResetItem = new JMenuItem("Reset Board");
+		JMenuItem quitGameItem = new JMenuItem("Menu");
+		JMenuItem quitProgramItem = new JMenuItem("Desktop");
+		
+		//Set JMenuBar appearance
 		gameOptionsBar.setBackground(boardColorDark);
 		gameOptionsMenu.setForeground(Color.WHITE);
 		gameOptionsMenu.setBackground(boardColorDark);
@@ -154,106 +147,92 @@ public class AbaloneMenu extends JFrame
 		quitProgramItem.setForeground(Color.RED);
 		quitProgramItem.setBackground(boardColorDark);
 		
+		//Add Action Listeners to JMenuBar components
+		quitGameItem.addActionListener(actionListener);
+		quitProgramItem.addActionListener(actionListener);
+		showRulesItem.addActionListener(actionListener);
+		gameResetItem.addActionListener(actionListener);
+		
+		//Build JMenuBar
 		gameOptionsBar.add(gameOptionsMenu);
 		gameOptionsMenu.add(showRulesItem);
 		gameOptionsMenu.add(gameResetItem);
 		gameOptionsMenu.add(gameQuitMenu);
 		gameQuitMenu.add(quitGameItem);
 		gameQuitMenu.add(quitProgramItem);
-		
-		quitGameItem.addActionListener(MainListener);
-		quitProgramItem.addActionListener(MainListener);
-		showRulesItem.addActionListener(MainListener);
-		gameResetItem.addActionListener(MainListener);
-		addComponentListener(ComponentListener);
+	}
 
-		RulesPanelSetup();
-		GamePanelSetup();
-
-		setName("MenuFrame");
-		setContentPane(menuPanel);
-		setTitle("Abalone");
-		setSize(screenSize);
-		setMinimumSize(new Dimension((int)(screenWidth/3), (int)(screenHeight*0.85)));
-		setBackground(boardColorLight);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-	}
-	
-	public static void main(String[] args)
+	private void goodByePanelSetup()
 	{
-		AbaloneMenu AbaloneMenu = new AbaloneMenu();
-	}
-	
-	public void RulesPanelSetup()
-	{
-		rulesPanel = new RulesPanel();
+		//Set goodByePanel appearance
+		goodByePanel.setLayout(new BorderLayout());
+		goodByePanel.setBackground(boardColorLight);
 		
-		backToMenuButton = new JButton("Back");
-		backToMenuButton.setForeground(Color.RED);
-		backToMenuButton.setBackground(boardColorDark);
-		backToMenuButton.addActionListener(MainListener);
-		rulesScrollPane = new JScrollPane(rulesPanel);
-		rulesScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		rulesPanel.add(backToMenuButton);
+		//Set goodByeLabel Appearance
+		goodByeLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, getHeight()/10));
+		goodByeLabel.setForeground(boardColorDark);
+
+		//Instance of the game panel for display on goodByePanel
+		AbalonePanel abalonePanelD = new AbalonePanel(new AbaloneGraph(), false, true);
+
+		//Build goodByePanel
+		goodByePanel.add(goodByeLabel, BorderLayout.NORTH);
+		goodByePanel.add(abalonePanelD, BorderLayout.CENTER);
 	}
-	
-	public void GamePanelSetup()
+
+	//Image wouldn't update until resizing the window, so I have it resize to the same size
+	private void updateImage()
 	{
-		abaloneGraphS = new AbaloneGraph();
-		abalonePanelS = new AbalonePanel(abaloneGraphS, true, false);
-		abaloneGraphM = new AbaloneGraph();
-		abalonePanelM = new AbalonePanel(abaloneGraphM, false, false);
+		repaint();
+		setSize(getWidth()-1, getHeight()-1);
+		setSize(getWidth()+1, getHeight()+1);
 	}
 	
-	ActionListener MainListener = new ActionListener()
+	//Action listener for buttons
+	private ActionListener actionListener = new ActionListener()
 	{ 
+		Sound sound = new Sound();
 		public void actionPerformed(ActionEvent actionEvent) 
 	    {
-			//System.out.println("Player says " + actionEvent.getActionCommand());
+			//Sound plays whenever a button is pressed
 			sound.setFile(0);
         	sound.play();
+			
+			//"Singleplayer" button in main menu
 			if (actionEvent.getActionCommand().equals("Singleplayer"))
 			{
 				setContentPane(abalonePanelS);
 				setJMenuBar(gameOptionsBar);
 				setTitle("Abalone: Singleplayer");
-				repaint();
 				spGameInProgress = true;
-				setSize(getWidth()-1, getHeight()-1);
-				setSize(getWidth()+1, getHeight()+1);
+				updateImage();
 			}
+			//"Multiplayer" button in main menu
 			else if (actionEvent.getActionCommand().equals("Multiplayer"))
 			{
 				setContentPane(abalonePanelM);
 				setJMenuBar(gameOptionsBar);
 				setTitle("Abalone: Multiplayer");
-				repaint();
 				mpGameInProgress = true;
-				setSize(getWidth()-1, getHeight()-1);
-				setSize(getWidth()+1, getHeight()+1);
+				updateImage();
 			}
+			//"Rules" button found in main menu or in game panel JMenuBar
 			else if (actionEvent.getActionCommand().equals("Rules"))
 			{
-				setJMenuBar(null);
 				setContentPane(rulesScrollPane);
+				setJMenuBar(null);
 				setTitle("Abalone: Rules");
-				repaint();
-				setSize(getWidth()-1, getHeight()-1);
-				setSize(getWidth()+1, getHeight()+1);
+				updateImage();
 			}
+			//Main menu "quit" button or game panel JMenuBar "Quit to... Desktop" button
+			//Displays goodByePanel for 2 seconds before closing program
 			else if (actionEvent.getActionCommand().equals("Quit") || actionEvent.getActionCommand().equals("Desktop"))
 			{
-				setContentPane(menuPanel);
+				setContentPane(goodByePanel);
 				setJMenuBar(null);
-				menuPanel.remove(buttonSectionPanel);
-				title.setText("Thanks for playing!");
-				
-				repaint();
-				setSize(getWidth()-1, getHeight()-1);
-				setSize(getWidth()+1, getHeight()+1);
+				updateImage();
 
-				Timer timer = new Timer(3000, new ActionListener()
+				Timer timer = new Timer(2000, new ActionListener()
 				{
 					@Override
 					public void actionPerformed(ActionEvent e) {
@@ -263,6 +242,7 @@ public class AbaloneMenu extends JFrame
 				timer.setRepeats(false);
 				timer.start();
 			}
+			//RulesPanel "Back" button
 			else if (actionEvent.getActionCommand().equals("Back"))
 			{
 				if (spGameInProgress)
@@ -283,65 +263,45 @@ public class AbaloneMenu extends JFrame
 					setContentPane(menuPanel);
 					setTitle("Abalone");
 				}
-				repaint();
-				setSize(getWidth()-1, getHeight()-1);
-				setSize(getWidth()+1, getHeight()+1);
+				updateImage();
 			}
+			//"Quit to... Menu" button found in game panel JMenuBar
 			else if (actionEvent.getActionCommand().equals("Menu"))
 			{
-				if (spGameInProgress)
-				{
-					spGameInProgress = false;
-				}
-				if (mpGameInProgress)
-				{
-					mpGameInProgress = false;
-				}
-				
+				spGameInProgress = false;
+				mpGameInProgress = false;
 				setContentPane(menuPanel);
 				setJMenuBar(null);
 				setTitle("Abalone");
-				repaint();
-				setSize(getWidth()-1, getHeight()-1);
-				setSize(getWidth()+1, getHeight()+1);
+				updateImage();
 			}
-			else if (actionEvent.getActionCommand().equals("Reset Game"))
+			//"Reset Board" button found in game panel JMenuBar
+			//Resets whichever board is currently being displayed
+			else if (actionEvent.getActionCommand().equals("Reset Board"))
 			{
 				if (spGameInProgress)
 				{
-					abaloneGraphS = new AbaloneGraph();
-					abalonePanelS = new AbalonePanel(abaloneGraphS, true, false);
+					abalonePanelS = new AbalonePanel(new AbaloneGraph(), true, false);
 					setContentPane(abalonePanelS);
 				}
 				if (mpGameInProgress)
 				{
-					abaloneGraphM = new AbaloneGraph();
-					abalonePanelM = new AbalonePanel(abaloneGraphM, false, false);
+					abalonePanelM = new AbalonePanel(new AbaloneGraph(), false, false);
 					setContentPane(abalonePanelM);
 				}
-				repaint();
-				setSize(getWidth()-1, getHeight()-1);
-				setSize(getWidth()+1, getHeight()+1);
+				updateImage();
 			}
 	    }
 	};
 
-	ComponentAdapter ComponentListener = new ComponentAdapter()
+	//Component listener for JFrame
+	private ComponentAdapter componentListener = new ComponentAdapter()
 	{	
-		public void componentShown(ComponentEvent ce)
-		{
-
-		}
-
-		public void componentHidden(ComponentEvent ce)
-		{
-
-		}
-
+		//Resizes titleLabel and goodByeLabel as JFrame is resized
 		public void componentResized(ComponentEvent ce)
 		{
-			title.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, menuPanel.getHeight()/10));
-			menuPanel.setPreferredSize(new Dimension(getWidth(), getHeight()-90));
+			titleLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, menuPanel.getHeight()/10));
+			goodByeLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, getHeight()/10));
 		}
 	};
 }
